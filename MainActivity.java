69c7e26f68a7e53e659ffc5721c05050a1e09884
joabeplay1 <joabejonatas186@gmail.com>
2026-05-22 15:04:03 -0300

    Refactor MainActivity to use play button only
    Removed unused buttons and updated click listener for play button.

--- MainActivity.java
+++ MainActivity.java
@@ -1,49 +1,31 @@
-package com.example.arcadefliperama;
+package com.example.sinuca;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button metalSlug;
-    Button kof;
+    Button play;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        metalSlug = findViewById(R.id.btnMetalSlug);
-        kof = findViewById(R.id.btnKOF);
+        play = findViewById(R.id.btnPlay);
 
-        metalSlug.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        play.setOnClickListener(v -> {
 
-                Intent intent =
+            Intent intent =
                     getPackageManager()
                     .getLaunchIntentForPackage(
-                    "com.seleuco.mame4d2024");
+                            "com.seleuco.mame4d2024");
 
-                startActivity(intent);
-            }
-        });
+            startActivity(intent);
 
-        kof.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent intent =
-                    getPackageManager()
-                    .getLaunchIntentForPackage(
-                    "com.seleuco.mame4d2024");
-
-                startActivity(intent);
-            }
         });
     }
 }
